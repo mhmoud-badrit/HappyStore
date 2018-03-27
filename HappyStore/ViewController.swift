3a01fd3c1d21b0b2f92266f0d04c8841e629d542
Mahmoud El-Naggar <elnaggaralex@hotmail.com>
2018-03-28 00:29:44 +0200

    Initial Commit

---
+++ HappyStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyStore
+//
+//  Created by Mahmoud El-Naggar on 3/28/18.
+//  Copyright © 2018 Mahmoud El-Naggar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
